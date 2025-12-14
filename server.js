d56662b48b858f343081548d8788cca28539168f
atshybrid <amoghnyatechsolutions@gmail.com>
2025-12-15 01:47:15 +0530

    chore: add Node entrypoint shims for Render

---
+++ server.js
@@ -0,0 +1,16 @@
+// Compatibility entrypoint for platforms still starting `node server.js`.
+// The real compiled server lives at `dist/server.js`.
+
+try {
+  // eslint-disable-next-line global-require
+  require('./dist/server.js');
+} catch (err) {
+  if (err && err.code === 'MODULE_NOT_FOUND') {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Cannot find ./dist/server.js. Did you run `npm run build` before starting?\n' +
+        'On Render, set Build Command: `npm ci; npm run build` and Start Command: `npm start`.'
+    );
+  }
+  throw err;
+}
